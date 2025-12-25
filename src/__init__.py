4963f5fe0ece3a502abf911e64d93df4af99649f
MelvinDY <melvindarialyogiana@gmail.com>
2025-12-26 01:02:09 +0700

    Add src package init with version info

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Jalanlytics - Street Income Intelligence
+
+Analyze CCTV footage of Indonesian streets to estimate income levels
+by detecting, classifying, and tracking vehicles.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Jalanlytics Team"
